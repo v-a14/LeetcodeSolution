91218d79c02036d0be6ca8eefac6d876ea5cf863
Vaibhav Agarwal <55106600+v-a14@users.noreply.github.com>
2022-09-07 17:45:32 +0530

    Time: 56 ms (49.92%), Space: 18.3 MB (12.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,13 +3,22 @@ public:
     vector<int> dx = {1,0,-1,0};
     vector<int> dy = {0,1,0,-1};
     int n,m;
-    void dfs(vector<vector<char>>&grid, vector<vector<bool>>&vis, int x, int y){
-        if(x<0 || y<0 || x>=n|| y>=m || vis[x][y] || grid[x][y]!='1') return;
+    void bfs(vector<vector<char>>&grid, vector<vector<bool>>&vis, int x, int y){
+        queue<pair<int,int>>q;
+        q.push({x, y});
         vis[x][y] = true;
-        for(int i=0;i<4;i++){
-            int tempX = x + dx[i];
-            int tempY = y + dy[i];
-            dfs(grid, vis, tempX, tempY);
+        while(!q.empty()){
+            int currX = q.front().first;
+            int currY = q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int tempX = currX + dx[i];
+                int tempY = currY + dy[i];
+                if(tempX>=0 && tempX<n && tempY>=0 && tempY<m && grid[tempX][tempY] == '1' && vis[tempX][tempY] == false){
+                    q.push({tempX, tempY});
+                    vis[tempX][tempY] = true;
+                }
+            }
         }
         return;
     }
@@ -22,7 +31,7 @@ public:
             for(int j=0;j<m;j++){
                 if(vis[i][j] == false && grid[i][j] == '1'){
                     count++;
-                    dfs(grid, vis, i, j);
+                    bfs(grid, vis, i, j);
                 }
             }
         }
