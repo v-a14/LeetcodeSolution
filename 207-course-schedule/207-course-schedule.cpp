1b107a71f2a76b9b0729e1e44c3d466fa1f0f0a1
Vaibhav Agarwal <55106600+v-a14@users.noreply.github.com>
2022-09-07 17:16:48 +0530

    Time: 53 ms (16.97%), Space: 12.8 MB (99.23%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -17,19 +17,29 @@ public:
     }
     bool canFinish(int n, vector<vector<int>>& edges) {
         vector<int>adj[n];
+        vector<int>indegree(n, 0);
         for(int i=0;i<edges.size();i++){
             int start = edges[i][0];
             int end = edges[i][1];
+            indegree[end]++;
             adj[start].push_back(end);
         }
-        vector<bool>dfsVisit(n, false);
-        vector<bool>vis(n, false);
-        bool flag = false;
+        queue<int>q;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                flag |= hasCycle(adj, vis, dfsVisit, i);
+            if(indegree[i] == 0) q.push(i);
+        }
+        int count = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            count++;
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
             }
         }
-        return !flag;
+        return count == n;
     }
 };
