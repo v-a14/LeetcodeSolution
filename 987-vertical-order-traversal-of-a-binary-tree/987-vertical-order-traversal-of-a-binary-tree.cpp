449eaa32525b83645f3a1a5439983b36326cd91f
Vaibhav Agarwal <55106600+v-a14@users.noreply.github.com>
2022-09-07 20:59:40 +0530

    Time: 8 ms (58.97%), Space: 12.1 MB (83.46%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,36 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // map chaiye jo har motai ke liye, vector mei data daalta jaaega 
+        queue<pair<TreeNode*, int>>q;
+        q.push({root,0});
         map<int, vector<int>>mp;
-        // queue chaiye h ek to store node, aur uski motai kitni h 
-        queue<pair<TreeNode*,int>>q;
-        q.push({root, 0});
         while(!q.empty()){
             int len = q.size();
-            multiset<pair<int,int>>s;
-            while(len--){
-                TreeNode *curr = q.front().first;
-                int width = q.front().second;
+            map<int,vector<int>>mp1;
+            for(int i=0;i<len;i++){
+                TreeNode *currNode = q.front().first;
+                int dis = q.front().second;
                 q.pop();
-                s.insert({curr->val, width});
-                // mp[width].push_back(curr->val);
-                if(curr->left){
-                    q.push({curr->left, width-1});
-                }
-                if(curr->right){
-                    q.push({curr->right, width+1});
+                mp1[dis].push_back(currNode->val);
+                if(currNode->left) q.push({currNode->left, dis-1});
+                if(currNode->right) q.push({currNode->right, dis+1});
+            }
+            for(auto it = mp1.begin();it!=mp1.end();it++){
+                int dis = it->first;
+                sort(it->second.begin(), it->second.end());
+                for(auto it1 : it->second){
+                    mp[dis].push_back(it1);
                 }
             }
-            for(auto it:s){
-                mp[it.second].push_back(it.first);
+           
+        }
+         vector<vector<int>>ans;
+            for(auto it:mp){
+                ans.push_back(it.second);
             }
-        }
-        vector<vector<int>>ans;
-        for(auto it:mp){
-            // sort(it.second.begin(), it.second.end());
-            ans.push_back(it.second);
-        }
         return ans;
     }
 };
