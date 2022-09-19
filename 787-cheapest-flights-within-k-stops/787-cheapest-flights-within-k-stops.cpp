17869fdf99917869406dcdde020ca7f9f6ee3d54
Vaibhav Agarwal <55106600+v-a14@users.noreply.github.com>
2022-09-19 18:07:19 +0530

    Time: 28 ms (91.93%), Space: 13.4 MB (63.86%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -5,32 +5,30 @@ public:
         for(int i=0;i<flights.size();i++){
             int start = flights[i][0];
             int end = flights[i][1];
-            int cost = flights[i][2];
-            adj[start].push_back({end, cost});
+            int weight = flights[i][2];
+            adj[start].push_back({end, weight});
         }
-        vector<int>stops(n, 1e9);
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>pq;
+        pq.push({0, src, 0});
         vector<int>dis(n, 1e9);
+        vector<int>stops(n, 1e9);
         dis[src] = 0;
         stops[src] = 0;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>q;
-        // current cost, starting vertex, current stops 
-        q.push({0, src, 0});
-        while(!q.empty()){
-            int currCost = q.top()[0];
-            int currNode = q.top()[1];
-            int currStops = q.top()[2];
-            if(currNode == dst) return currCost;
-            // cout << currNode << endl;
-            q.pop();
-            if(currStops>k) continue;
-            for(auto it:adj[currNode]){
-                if((dis[it.first] > currCost + it.second) || currStops+1 < stops[it.first]){
-                    stops[it.first] = currStops+1;
-                    dis[it.first] = currCost + it.second;
-                    q.push({dis[it.first], it.first, stops[it.first]});
+        while(!pq.empty()){
+            int node = pq.top()[1];
+            int currWeight = pq.top()[0];
+            int currStops = pq.top()[2];
+            if(node == dst) return currWeight; 
+            pq.pop();
+            if(currStops > k) continue;
+            for(auto it:adj[node]){
+                if((dis[it.first] > currWeight + it.second)  || currStops+1<stops[it.first]){
+                    dis[it.first] = currWeight + it.second;
+                    stops[it.first] = currStops + 1;
+                    pq.push({dis[it.first], it.first, stops[it.first]});
                 }
             }
         }
-        return dis[dst] == 1e9 ? -1 : dis[dst];
+        return -1;
     }
 };
