b838f4e41244f5b4cfd82fdf4ce66b591db9f6c1
Vaibhav Agarwal <55106600+v-a14@users.noreply.github.com>
2022-09-20 12:21:52 +0530

    Time: 3 ms (78.65%), Space: 7.7 MB (22.29%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int helper(vector<int>&nums, int start, int end){
+    int helper(vector<int>&nums, int start, int end, vector<vector<int>>&dp){
         if(start>end) return 0;
-        int a = nums[start] + min(helper(nums, start+2, end), helper(nums, start+1, end-1));
-        int b = nums[end] + min(helper(nums,start+1, end-1), helper(nums, start, end-2));
-        return max(a,b);
+        if(dp[start][end]!=-1) return dp[start][end];
+        int a = nums[start] + min(helper(nums, start+2, end, dp), helper(nums, start+1, end-1, dp));
+        int b = nums[end] + min(helper(nums,start+1, end-1, dp), helper(nums, start, end-2, dp));
+        return dp[start][end] = max(a,b);
     }
     bool PredictTheWinner(vector<int>& nums) {
         int sum = 0;
         for(auto it:nums) sum += it;
-        int val = helper(nums, 0, nums.size()-1);
+        int n = nums.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        int val = helper(nums, 0, nums.size()-1, dp);
         if(val >= (sum + 1)/2) return true;
         return false;
     }
