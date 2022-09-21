a0a21fd854db156252a2e6c25c3a439d5cd5a9cf
Vaibhav Agarwal <55106600+v-a14@users.noreply.github.com>
2022-09-22 01:08:41 +0530

    Time: 91 ms (46.49%), Space: 29.1 MB (65.49%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -11,32 +11,28 @@ class Codec {
 public:
 
     // Encodes a tree to a single string.
-    // kuch nhi krenge bas null k jagah '#' daal denge, aur ek value ke baad ',' jisse separate kr skte 
     string serialize(TreeNode* root) {
         if(!root) return "";
+        string encode;
         queue<TreeNode*>q;
         q.push(root);
-        string str = "";
         while(!q.empty()){
-            TreeNode *currNode = q.front();
+            TreeNode *curr = q.front();
             q.pop();
-            if(currNode)
-                str += to_string(currNode->val);
-            else str += '#';
-            str += ',';
-            if(currNode){
-                q.push(currNode->left);
-                q.push(currNode->right);
+            if(curr == NULL){
+                encode += "#,";
+                continue;
+            }else{
+                encode += to_string(curr->val);
+                encode += ',';
             }
+            q.push(curr->left);
+            q.push(curr->right);
         }
-            return str;
+        return encode;
     }
 
-
     // Decodes your encoded data to tree.
-    // string ko parse kr lenge, aur har ek ',' isse pehle ka pura element process krenge 
-    // agar null hoga, toh matlab hamare pass '#' aaya hoga, toh bas hum uss node mei 
-    // null daal denge 
     TreeNode* deserialize(string data) {
         if(data.size() == 0) return NULL;
         stringstream s(data);
@@ -46,24 +42,17 @@ public:
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
-            TreeNode *currNode = q.front();
+            TreeNode *curr = q.front();
             q.pop();
-            // checking the left part of the current node
-            // if it is "#", then add null, other wise add the number 
             getline(s, str, ',');
-            if(str == "#"){
-                currNode->left = NULL;
-            }else{
-                currNode->left = new TreeNode(stoi(str));
-                q.push(currNode->left);
+            if(str != "#"){ 
+                curr->left = new TreeNode(stoi(str));
+                q.push(curr->left);
             }
-            
             getline(s, str, ',');
-            if(str == "#"){
-                currNode->right = NULL;
-            }else{
-                currNode->right = new TreeNode(stoi(str));
-                q.push(currNode->right);
+            if(str != "#"){
+                curr->right = new TreeNode(stoi(str));
+                q.push(curr->right);
             }
         }
         return root;
