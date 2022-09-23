73faae9261fc7997621e39428d9e46568aa5abb8
Vaibhav Agarwal <55106600+v-a14@users.noreply.github.com>
2022-09-23 09:36:37 +0530

    Time: 28 ms (18.32%), Space: 21.7 MB (30.65%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -9,19 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#define ll long long 
 class Solution {
 public:
-    bool helper(TreeNode *root, ll minVal, ll maxVal){
+    bool helper(TreeNode *root, long long minVal, long long maxVal){
         if(!root) return true;
         if(root->val < minVal || root->val > maxVal) return false;
-        bool Left = helper(root->left, minVal, (ll)root->val-1);
-        if(!Left) return false;
-        bool Right = helper(root->right, (ll)root->val+1, maxVal);
-        if(!Right) return false;
+        bool left =  helper(root->left, minVal, (long long)root->val-1);
+        if(!left) return left;
+        bool right = helper(root->right, (long long)root->val+1, maxVal);
+        if(!right) return right;
         return true;
     }
     bool isValidBST(TreeNode* root) {
-        return helper(root, LLONG_MIN, LLONG_MAX);
+        return helper(root, INT_MIN, INT_MAX);
     }
 };
